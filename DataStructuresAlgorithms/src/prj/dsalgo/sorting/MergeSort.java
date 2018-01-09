7015c92f67e9a430dba3dd0263f452ba3f67ac83
akhila <aksriniv@adobe.com>
2018-01-09 15:51:15 +0530

    Fixed  merge sort

--- DataStructuresAlgorithms/src/prj/dsalgo/sorting/MergeSort.java
+++ DataStructuresAlgorithms/src/prj/dsalgo/sorting/MergeSort.java
@@ -3,6 +3,8 @@
  */
 package prj.dsalgo.sorting;
 
+import java.util.Arrays;
+
 /**
  * @author akhilasrinivas
  *
@@ -14,32 +16,53 @@ public class MergeSort {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		 int[]  input={2,3,1,6,3,8,2,5,9,11,43,21,32,54,42};
-			int [] output=mergesort(input,0,input.length);
+	 int[]  input={2,3,1,6,3,8,2,5,9,11,43,21,32,54,42};
+//		 System.out.println(input.length-1);
+//			
+		mergesort(input,0,input.length-1);
+		System.out.println(Arrays.toString(input));
 	}
-static int[] merge(int[] a,int p,int q,int r) {
-	int[] left= {}; int[] right= {};int m=0,n=0,i=0,j=0;int[] result= {};
-	for(i=p;i<=(r-1);i++) {
-		left[m]=a[i]; m++;
+static void merge(int[] a,int p,int q,int r) {
+	int[] first= new int[20];
+	int[] second= new int[20];
+	int m=0,n=0,i=0,j=0;
+	for(i=p;i<=r;i++) {
+		first[m]=a[i]; m++;
 	}
-	for(j=r;j<=q;j++) {
-		right[n]=a[j]; n++;
+	for(j=r+1;j<=q;j++) {
+		second[n]=a[j]; n++;
 	}
-	i=j=0;
-	for(int k=0;k<(m+n);k++) {
-		if(left[i]<right[j]) {
-			result[k]=left[i];i++;}
-		else { result[k]=right[j];j++;}
+	i=0;
+	j=0;
+	int k=0;
+	for(k=p;k<=q;k++) {
+		if((i==m) || (j==n)) break;
+		if(first[i]<=second[j]) {
+			a[k]=first[i];
+			i++;
+			} else  { a[k]=second[j];
+			j++;}
 	}
-	
-	return result;
-	
-}
+	while(k<=q)
+	{
+		if(i<m){
+			a[k]=first[i];
+			i++;
+			}	
+			if(j<n){ a[k]=second[j];
+			j++;}
+		k++;
+		
+	}
+	}
 
-static int[] mergesort(int[] a,int p,int q) {
-	int r=(p+q)/2;
-	mergesort(a,p,r-1);
-	mergesort(a,r,q);
-	return merge(a,p,q,r);
-}
+static void mergesort(int[] a,int p,int q) {
+	int r=0;
+	if(p<q) {
+	 r=(p+q)/2;
+	mergesort(a,p,r);
+	mergesort(a,r+1,q);
+	merge(a,p,q,r);
+	}
+	}
 }
---
+++ DataStructuresAlgorithms/src/prj/dsalgo/sorting/Test.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package prj.dsalgo.sorting;
+
+import java.util.Arrays;
+
+/**
+ * @author akhilasrinivas
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int [] test= {2,4,5};
+test[1]=7;
+System.out.println(test.length);
+System.out.println(Arrays.toString(test));
+
+	}
+}
